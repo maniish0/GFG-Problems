18d72da31cdb9db97218c5499ec43bb6c3db1aaf
MANISH <manishpatel56709@gmail.com>
2025-09-19 19:48:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Selection Sort/selection-sort.cpp
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -3,16 +3,19 @@ class Solution {
     void selectionSort(vector<int> &arr) {
         int n = arr.size();
 
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
+        for (int i = 0; i < n - 1; ++i) {
+            int min_idx = i;
 
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
+            for (int j = i + 1; j < n; ++j) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
                 }
             }
 
-            swap(arr[i], arr[minIndex]);
+       
+            if (min_idx != i) {
+                swap(arr[i], arr[min_idx]);
+            }
         }
     }
 };
